2759fd73f3320efcbb1244c65cf44f2c9f76fa28
fantastickford1 <carlitos_zenteno@hotmail.com>
2017-04-23 23:43:41 -0500

    db

---
+++ KDBCR.sql
@@ -0,0 +1,188 @@
+CREATE DATABASE  IF NOT EXISTS `login` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `login`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: login
+-- ------------------------------------------------------
+-- Server version	5.7.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Pais`
+--
+
+DROP TABLE IF EXISTS `Pais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pais` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pais`
+--
+
+LOCK TABLES `Pais` WRITE;
+/*!40000 ALTER TABLE `Pais` DISABLE KEYS */;
+INSERT INTO `Pais` VALUES (1,'Mexico','MejorLugarEver');
+/*!40000 ALTER TABLE `Pais` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Roles`
+--
+
+DROP TABLE IF EXISTS `Roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Roles` (
+  `idRol` bigint(20) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idRol`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Roles`
+--
+
+LOCK TABLES `Roles` WRITE;
+/*!40000 ALTER TABLE `Roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tareas`
+--
+
+DROP TABLE IF EXISTS `Tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tareas` (
+  `idTareas` int(11) NOT NULL AUTO_INCREMENT,
+  `TareaNombre` varchar(45) DEFAULT NULL,
+  `TareaDescripcion` varchar(45) DEFAULT NULL,
+  `TareaEstado` varchar(45) DEFAULT NULL,
+  `nombreUsuario` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idTareas`),
+  UNIQUE KEY `TareaNombre_UNIQUE` (`TareaNombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tareas`
+--
+
+LOCK TABLES `Tareas` WRITE;
+/*!40000 ALTER TABLE `Tareas` DISABLE KEYS */;
+INSERT INTO `Tareas` VALUES (3,'Dam','helow','incompleto','yo'),(4,'Hacer la tarea','terminar manto',':v','fantastickford1'),(5,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `Tareas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `UsuarioRol`
+--
+
+DROP TABLE IF EXISTS `UsuarioRol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UsuarioRol` (
+  `idUsuario` bigint(20) NOT NULL,
+  `idRol` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `UsuarioRol`
+--
+
+LOCK TABLES `UsuarioRol` WRITE;
+/*!40000 ALTER TABLE `UsuarioRol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `UsuarioRol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuarios`
+--
+
+DROP TABLE IF EXISTS `Usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuarios` (
+  `idUsuario` bigint(20) NOT NULL,
+  `nombrePrefijo` varchar(255) DEFAULT NULL,
+  `nombreNombre` varchar(255) DEFAULT NULL,
+  `nombreApellidoPaterno` varchar(255) DEFAULT NULL,
+  `nombreApellidoMaterno` varchar(255) DEFAULT NULL,
+  `nombrePosfijo` varchar(255) DEFAULT NULL,
+  `nombreIniciales` varchar(255) DEFAULT NULL,
+  `nombreUsuario` varchar(255) NOT NULL,
+  `claveAcceso` varchar(255) NOT NULL,
+  `lng` float DEFAULT NULL,
+  `lat` float DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `nombreUsuario` (`nombreUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuarios`
+--
+
+LOCK TABLES `Usuarios` WRITE;
+/*!40000 ALTER TABLE `Usuarios` DISABLE KEYS */;
+INSERT INTO `Usuarios` VALUES (1,'C','Carlos','Zenteno','Robles','A','CAZR','fantastickford1','SithLordKing16',0,0);
+/*!40000 ALTER TABLE `Usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sugerencias`
+--
+
+DROP TABLE IF EXISTS `sugerencias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sugerencias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `sugerencia` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sugerencias`
+--
+
+LOCK TABLES `sugerencias` WRITE;
+/*!40000 ALTER TABLE `sugerencias` DISABLE KEYS */;
+INSERT INTO `sugerencias` VALUES (1,'WL','asd');
+/*!40000 ALTER TABLE `sugerencias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-23 23:41:08
